57cac86fe4b6968766c130cb514d7d10c6349030
Null <kelanidarasimi9@gmail.com>
2024-06-24 17:36:08 +0100

    task 0 redid

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,4 +5,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """index the page"""
-    return (page, page_size)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
