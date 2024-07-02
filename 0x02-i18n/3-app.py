d3911b4e3225433036d936c47de4a25abeb8b3fa
Null <kelanidarasimi9@gmail.com>
2024-07-02 21:44:03 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 
@@ -11,11 +12,18 @@ class Config:
 
 app.config.from_object(Config)
 
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route("/")
 def index():
     """
     index view for renderting static html
     """
-    return render_template("1-index.html")
-
+    context = {'home_title': ''}
+    return render_template("2-index.html")
