4477464496aa485b6193a9d8ef71e6c60ae4f793
Null <kelanidarasimi9@gmail.com>
2024-07-02 18:40:05 +0100

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+a simple flask application for renderng a templates
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+app.route('/')
+def index():
+    """
+    index view for renderting static html
+    """
+    return render_template('0-index.html')
